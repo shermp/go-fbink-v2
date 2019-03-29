256bd14ce38705723c98990562da84eddfe7c5a5
NiLuJe <ninuje@gmail.com>
2019-03-29 23:42:55 +0100

    Fix the example program ;).

--- example/main.go
+++ example/main.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/shermp/go-fbink/gofbink"
+	"github.com/shermp/go-fbink-v2/gofbink"
 )
 
 func testPrints(fb *gofbink.FBInk, cfg *gofbink.FBInkConfig) {
@@ -12,14 +12,16 @@ func testPrints(fb *gofbink.FBInk, cfg *gofbink.FBInkConfig) {
 	for i := 0; i < 10; i++ {
 		s := fmt.Sprintf("Test line %d", i)
 		fb.Println(s)
+		fmt.Println(s)
 		time.Sleep(500 * time.Millisecond)
 	}
 	// Lets test last line replacement next
 	fb.PrintLastLn("This should update the last line!")
 	time.Sleep(1 * time.Second)
 	// And we finish with a nice progress bar :)
-	for i := 0; i < 100; i += 10 {
+	for i := 0; i <= 100; i += 10 {
 		fb.PrintProgressBar(uint8(i), cfg)
+		fmt.Println("Progress bar @", i, "%")
 		time.Sleep(500 * time.Millisecond)
 	}
 }
