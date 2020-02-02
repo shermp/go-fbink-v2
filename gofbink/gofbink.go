1412ca0c711225e8d3bb6220efd0a5f06b106830
NiLuJe <ninuje@gmail.com>
2020-02-02 02:47:12 +0100

    Vague attempt at implementing GetLastRect
    Quite possibly wrong ^^.

--- gofbink/gofbink.go
+++ gofbink/gofbink.go
@@ -763,6 +763,17 @@ func (f *FBInk) PrintRBGA(xOff, yOff int16, im *image.RGBA, cfg *FBInkConfig) er
 	return createError(res)
 }
 
+
+// GetLastRect returns the last painted to area
+// See "fbink.h" for detailed usage and explanation
+func (f *FBInk) GetLastRect(rect *FBInkRect) {
+	rectC := C.fbink_get_last_rect()
+	rect.Left = uint16(rectC.left)
+	rect.Top = uint16(rectC.top)
+	rect.Width = uint16(rectC.width)
+	rect.Height = uint16(rectC.height)
+}
+
 // ClearScreen simply clears the screen to white
 // See "fbink.h" for detailed usage and explanation
 func (f *FBInk) ClearScreen(cfg *FBInkConfig, rect *FBInkRect) error {
@@ -773,7 +784,6 @@ func (f *FBInk) ClearScreen(cfg *FBInkConfig, rect *FBInkRect) error {
 }
 
 // TODO: fbink_dump, fbink_region_dump, fbink_restore, fbink_free_dump_data
-// TODO: fbink_get_last_rect
 
 // ButtonScan will scan for the 'Connect' button on the Kobo USB connect screen
 // See "fbink.h" for detailed usage and explanation
